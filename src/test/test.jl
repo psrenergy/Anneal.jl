6ec08308f0333c8ba8dc922b071d896590c466fc
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-10-24 10:20:07 -0300

    Bump Version

--- src/test/test.jl
+++ src/test/test.jl
@@ -1,10 +1,15 @@
+# -*- Interface Tests -*- #
 include("interface/moi.jl")
 include("interface/anneal.jl")
 
+# -*- Example Tests -*- #
 include("examples/basic.jl")
 
 @doc raw"""
 """ function test end
+
+@doc raw"""
+""" function test_config! end
 
 function Anneal.test(optimizer::Type{<:AbstractSampler}; examples::Bool=false)
     Test.@testset "-*- Interface" verbose = true begin
