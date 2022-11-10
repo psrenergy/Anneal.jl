493de86870bae6fcaeb933440f9d8c56603bf4e9
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-11-10 10:21:13 -0300

    Add `get` for `PrimalStatus` and `DualStatus`

--- src/interface/abstract/wrapper.jl
+++ src/interface/abstract/wrapper.jl
@@ -29,12 +29,26 @@ MOI.supports(
     ::MOI.ObjectiveFunction{<:Union{SQF{T}, SAF{T}, VI}}
 ) where {T} = true
 
-# ~ By default, all samplers are their own raw solvers.
+# By default, all samplers are their own raw solvers.
 MOI.get(sampler::AbstractSampler, ::MOI.RawSolver) = sampler
+
+# Since problems are unconstrained, all available solutions are feasible.
+function MOI.get(sampler::AbstractSampler, ps::MOI.PrimalStatus)
+    n = MOI.get(sampler, MOI.ResultCount())
+    i = ps.result_index
+
+    if 1 <= i <= n
+        return MOI.FEASIBLE_POINT
+    else
+        return MOI.NO_SOLUTION
+    end
+end
+
+MOI.get(::AbstractSampler, ::MOI.DualStatus) = MOI.NO_SOLUTION
 
 # ~ Introduce `reads(model; result = i)` interface.
 #   The `reads` function is exported.
-QUBOTools.reads(model::JuMP.Model; result::Integer) = QUBOTools.reads(JuMP.unsafe_backend(model), result)
+QUBOTools.reads(model::JuMP.Model; result::Integer) = QUBOTools.reads(model, result)
 
 # ~ Give access to QUBOTools' queries.
 QUBOTools.backend(model::JuMP.Model) = QUBOTools.backend(JuMP.unsafe_backend(model))
