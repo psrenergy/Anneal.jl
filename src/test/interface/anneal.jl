5753fa02f3c2a3981745f60734be11099cb37335
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2022-11-10 14:34:47 -0300

    Enhance test display

--- src/test/interface/anneal.jl
+++ src/test/interface/anneal.jl
@@ -1,5 +1,12 @@
 function __test_anneal_interface(::Type{S}) where {S<:AbstractSampler}
-    Test.@testset "Anneal" verbose = true begin
+    Test.@testset "Anneal (Abstract)" verbose = true begin
         Test.@test hasmethod(Anneal.sample, (S,))
+    end
+end
+
+function __test_anneal_interface(::Type{S}) where {S<:AutomaticSampler}
+    Test.@testset "Anneal (Automatic)" verbose = true begin
+        Test.@test hasmethod(Anneal.sample, (S,))
+        Test.@test hasmethod(Anneal.backend, (S,))
     end
 end
--- src/test/test.jl
+++ src/test/test.jl
@@ -6,17 +6,22 @@ include("interface/anneal.jl")
 include("examples/basic.jl")
 
 @doc raw"""
+    test(optimizer::Type{S}; examples::Bool=false) where {S<:AbstractSampler}
 """ function test end
 
-function Anneal.test(optimizer::Type{<:AbstractSampler}; examples::Bool=false)
-    Test.@testset "-*- Interface" verbose = true begin
-        __test_moi_interface(optimizer)
-        __test_anneal_interface(optimizer)
-    end
+function Anneal.test(optimizer::Type{S}; examples::Bool=false) where {S<:AbstractSampler}
+    solver_name = MOI.get(optimizer(), MOI.SolverName())
 
-    if examples
-        Test.@testset "-*- Examples" verbose = true begin
-            __test_basic_examples(optimizer)
+    Test.@testset "~*~ Anneal Tests for $(solver_name) ~*~" verbose = true begin
+        Test.@testset "→ Interface" verbose = true begin
+            __test_moi_interface(optimizer)
+            __test_anneal_interface(optimizer)
+        end
+
+        if examples
+            Test.@testset "→ Examples" verbose = true begin
+                __test_basic_examples(optimizer)
+            end
         end
     end
 end
